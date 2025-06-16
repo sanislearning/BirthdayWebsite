b09f84a4ad3cc02d225fba6bf2438cfbb0ffca03
sanislearning <sanjaybiju10@gmail.com>
2025-06-16 20:51:46 +0530

    Decided to build the entire page first then bother with the design, added all elements and tags

--- birthday-site/src/pages/cute.jsx
+++ birthday-site/src/pages/cute.jsx
@@ -4,6 +4,10 @@ import './cute.css';
 import Firework from '../assets/fireworks.gif';
 import butterfly from '../assets/butterfly.gif';
 import panda from "../assets/dancingPanda.gif";
+import mouse from "../assets/noodleMouse.gif";
+import hearts from "../assets/Hearts.gif";
+import stars from "../assets/Stars.gif";
+
 function Cute() {
   const navigate = useNavigate();
 
@@ -17,37 +21,71 @@ function Cute() {
           Can't really blow up actual fireworks rn, the gif will have to do
         </div>
       </div>
-      {/* Semicircle Text */}
-      <div className="semi-circle-text"> 
-      <svg viewBox="0 0 1200 800" width="1000" height="400">
-        <defs>
-          <path
-            id="semiPath"
-            d="M 200,600 A 400,400 0 0 1 1000,600"
-            fill="transparent"
-          />
-        </defs>
-        <text className="birthday-text">
-          <textPath href="#semiPath" startOffset="50%" textAnchor="middle">
-            Happy Birthday Siva
-          </textPath>
-        </text>
-      </svg>
 
-
+      {/* Semicircle Text and Fireworks Container */}
+      <div className="semi-circle-container">
+        <svg viewBox="0 0 1000 500" width="100%" height="auto">
+          <defs>
+            <path
+              id="semiPath"
+              d="M 100,400 A 400,400 0 0 1 900,400"
+              fill="transparent"
+            />
+          </defs>
+          <text className="birthday-text">
+            <textPath href="#semiPath" startOffset="50%" textAnchor="middle">
+              Happy Birthday Siva
+            </textPath>
+          </text>
+        </svg>
+        <img src={Firework} alt="fireworks" className='fireworks' />
       </div>
-      <img src={Firework} alt="fireworks" className='fireworks' />
-      {/* Navigation Button */}
-      <button className="cute-button" onClick={() => navigate('/pro')}>
-        Click to be Professional 💼
-      </button>
-      <div class="initial-bottom">
-        <div class="happy-20">
+
+      
+
+      <div className="initial-bottom">
+        <img src={butterfly} alt="flying butterflies" className="butterfly"/>
+        <div className="happy-20">
           Happy 20th birthday to the prettiest person I know
         </div>
-      <img src={butterfly} alt="flying butterflies" className="butterfly"/>
-      <img src={panda} alt="Dancing panda" className="pandas"/>
+        <img src={panda} alt="Dancing panda" className="pandas"/>
       </div>
+
+      <div className="animal">
+        Found this gif while I was trying to design the site, this is you as an animal
+      </div>
+      
+      {/*The noodle mouse section*/}
+      <img src={mouse} alt="A mouse eating noodles" className='noodles'/>
+      <div className='noodleMouse'>
+        literally you
+      </div>
+
+      {/*Middle Center Text*/}
+      <div className="you">
+        You are quite honestly the kind of person, who, when life gives her lemons, look awkwardly at life and smile because you can’t really figure out what you should do. Thankfully your awkward smiles are one of the prettiest things about you. That works out pretty well.
+        I hope as the years continue, you will still smile at the things that trouble you and find your own way.
+      </div>
+
+      <div className="real-bottom">
+        <div className="hearts">
+          A bunch of hearts cause why not
+        </div>
+
+        <img src={hearts} alt="flying hearts" className='heart-gif'/>
+
+        <button className="cute-button" onClick={() => navigate('/pro')}>
+        Click to be Professional
+        </button>
+
+        <img src={stars} alt="rotating stars" className="stars"/>
+        <div className="song-selection">
+          I hope you enjoy my song selection, truly one of the greatest songs of all time
+        </div>
+
+
+      </div>
+
     </div>
   );
 }
